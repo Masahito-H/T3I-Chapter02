6f1ddc440671b192b5699510ade04741abb259c2
Masahito-H <masahito.hayashi.8p@gmail.com>
2020-06-16 20:49:33 +0900

    via atom

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,7 +21,7 @@ export default {
     },
     async asyncData({ app, store }){
     //warning: if this site will be deployed, I must change the above address.
-    const pdFile = await app.$axios.$get("https://5ee8b03e15fb50000721fe4c--festive-easley-218d4b.netlify.app/chapter2-0.pd");
+    const pdFile = await app.$axios.$get("https://festive-easley-218d4b.netlify.app/chapter2-0.pd");
     console.log(pdFile);
     store.dispatch("isLoading", false);
 
