342e2bfbe49a36a2f221cd7af4abe8bf5fc2f86f
Masahito-H <masahito.hayashi.8p@gmail.com>
2020-06-16 22:06:30 +0900

    via atom

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
-    titleTemplate: "%s - The Three Interactions";
+    titleTemplate: "%s - The Three Interactions",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
